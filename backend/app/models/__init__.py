1076f6085405ae39c6e910d33ecc87e8be647404
gianlutfiid <mentionlutfi@gmail.com>
2026-09-03 15:12:03 +0700

    Register service master models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -6,6 +6,16 @@ from app.models.base import Base
 from app.models.branch import Branch
 from app.models.customer import Customer
 from app.models.membership import BranchMembership
+from app.models.service import Service
+from app.models.service_category import ServiceCategory
 from app.models.user import User
 
-__all__ = ["Base", "Branch", "Customer", "User", "BranchMembership"]
+__all__ = [
+    "Base",
+    "Branch",
+    "Customer",
+    "User",
+    "BranchMembership",
+    "ServiceCategory",
+    "Service",
+]
