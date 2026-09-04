d67f05cfa7d89557bdb5fdc79decb95e8bb9c7ee
gianlutfiid <mentionlutfi@gmail.com>
2026-09-04 14:24:35 +0700

    Register KPI target model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -5,6 +5,7 @@ Import all models here so Alembic autogenerate can discover them.
 from app.models.base import Base
 from app.models.branch import Branch
 from app.models.customer import Customer
+from app.models.kpi_target import KpiTarget
 from app.models.membership import BranchMembership
 from app.models.order import Order, OrderItem
 from app.models.order_status_log import OrderStatusLog
@@ -15,16 +16,6 @@ from app.models.service_production_stage import ServiceProductionStage
 from app.models.user import User
 
 __all__ = [
-    "Base",
-    "Branch",
-    "Customer",
-    "User",
-    "BranchMembership",
-    "ServiceCategory",
-    "Service",
-    "ServiceProductionStage",
-    "Order",
-    "OrderItem",
-    "OrderStatusLog",
-    "ProductionJob",
+    "Base", "Branch", "Customer", "User", "BranchMembership", "ServiceCategory", "Service",
+    "ServiceProductionStage", "KpiTarget", "Order", "OrderItem", "OrderStatusLog", "ProductionJob",
 ]
